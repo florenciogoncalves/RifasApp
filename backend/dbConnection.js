5650e04de1676e2987f08c98f0b39749cf412b97
FlorencioGoncalves <florenciopaul@gmail.com>
2023-04-26 09:18:17 +0100

    De sequelize para mysql | Módulo

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,74 +1,21 @@
 const dbConnection = function () {
-	const Sequelize = require("sequelize");
-	const sequelize = new Sequelize("rifasApp", "root", "", {
+const mysql = require("mysql");
+
+// Configuração do banco de dados
+	const connection = mysql.createConnection({
 		host: "localhost",
-		dialect: "mysql",
+		user: "root",
+		password: "",
+		database: "rifasApp",
 	});
-
-	sequelize
-		.authenticate()
-		.then(function () {
-			console.log("Conectado com sucesso a base de dados!");
-		})
-		.catch(function (erro) {
-			console.log("Falha ao se conectar a base de dados!" + erro);
-		});
-
-	// Define as tabelas de menu por província
-	function setMenu(menu) {
-		const Menus = sequelize.define('menu_' + menu, {
-			id: {
-				type: Sequelize.INTEGER,
-				primaryKey: true,
-			},
-			nome: {
-				type: Sequelize.TEXT,
-			},
-			genero: {
-				type: Sequelize.STRING,
-			},
-      escolhido: {
-        type: Sequelize.BOOLEAN
-      }
-		});
-		Menus.sync({ force: true });
-	}
-
-  setMenu('luanda')
-  setMenu('saurimo')
-
-  const Usuarios = sequelize.define('usuarios', {
-			id: {
-				type: Sequelize.INTEGER,
-				primaryKey: true,
-			},
-			nome: {
-				type: Sequelize.TEXT,
-			},
-      passe: {
-        type: Sequelize.STRING
-      },
-			provincia: {
-				type: Sequelize.STRING,
-			},
-			genero: {
-				type: Sequelize.STRING,
-			},
-      selecionou: {
-        type: Sequelize.INTEGER
-      }
-		});
-		Usuarios.sync({ force: true });
-
-		const setUser = function(nome, passe, provincia, genero) {
-			Usuarios.create( {
-				nome: nome,
-				passe: passe,
-				provincia: provincia,
-				genero: genero,
-				selecionou: false	
-			})
+// Conexão com o banco de dados
+	connection.connect((err) => {
+		if (err) {
+			console.log("Erro ao conectar com o banco de dados:", err);
+			return;
 		}
+		console.log("Conexão com o banco de dados bem sucedida!");
+	});
 };
 
 module.exports = dbConnection;
